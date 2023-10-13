810a4881a83d2e50323e204ee1144c6c017ccd74
David Rosenberger <drose@Davids-MacBook-Pro.local>
2023-10-12 23:18:58 -0400

    restart on css

--- fucntions/main.js
+++ fucntions/main.js
@@ -46,32 +46,6 @@ $(function(){
 });
 
 
-//   Testimonals
-// ===============
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-}
-
 /* Open when someone clicks on the span element */
 function openNav() {
   document.getElementById("mainMenu").style.width = "100%";
